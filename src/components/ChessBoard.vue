2a1c2fd1be3fa3ee90faf9c756a5fd8fb7c36aba
Kevin Sherman <kevin@kshermphoto.com>
2018-03-15 05:57:16 -0500

    Add Chess board grid/styles

--- src/components/ChessBoard.vue
+++ src/components/ChessBoard.vue
@@ -1,11 +1,83 @@
 <template>
   <div class="chessboard-wrapper">
-    <h1>Chess is gr8!</h1>
+    <div class="chessboard-file-gutter">
+      <div class="gutter-file-labels">
+        <div
+          v-for="file in filesArray"
+          :key="`label-file-${file}`"
+          class="gutter-file-label"
+          v-html="file" />
+      </div>
+    </div>
+    <div class="chessboard-board">
+      <div class="gutter-rank-labels">
+        <div
+          v-for="rank in ranksArray"
+          :key="`label-rank-${rank}`"
+          class="gutter-rank-label"
+          v-html="rank" />
+      </div>
+      <div class="board-grid">
+        <div
+          v-for="rank in ranksArray"
+          :key="`board-rank-${rank}`"
+          class="board-rank">
+          <div
+            v-for="file in filesArray"
+            :key="`board-${file}${rank}`"
+            :class="{ active: currentSelection === `${file}${rank}` }"
+            class="board-square"
+            @click="logClick({file, rank})"
+            v-html="currentSelection === `${file}${rank}` ? `${file}${rank}` : null" />
+        </div>
+      </div>
+      <div class="gutter-rank-labels">
+        <div
+          v-for="rank in ranksArray"
+          :key="`label-rank-${rank}`"
+          class="gutter-rank-label"
+          v-html="rank" />
+      </div>
+    </div>
+    <div class="chessboard-file-gutter">
+      <div class="gutter-file-labels">
+        <div
+          v-for="file in filesArray"
+          :key="`label-file-${file}`"
+          class="gutter-file-label"
+          v-html="file" />
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+import { mapActions } from 'vuex';
 export default {
   name: 'ChessBoard',
+  data() {
+    return {
+      files: 'abcdefgh',
+      ranks: '12345678',
+      currentSelection: '',
+    };
+  },
+  computed: {
+    filesArray() {
+      return this.files.split('');
+    },
+    ranksArray() {
+      const splitArray = this.ranks.split('');
+      return splitArray.reverse();
+    },
+  },
+  methods: {
+    ...mapActions(['addMoveToHistory']),
+    logClick({ file, rank }) {
+      this.currentSelection = `${file}${rank}`;
+      const dateTimeStamp = Date.now();
+      this.addMoveToHistory({ file, rank, dateTimeStamp });
+    },
+  },
 };
 </script>
