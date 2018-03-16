1a52e6caa4860dbef453cb06583c0b76a71d8650
Kevin Sherman <kevin@kshermphoto.com>
2018-03-16 02:58:45 -0500

    Refactor chessboard style/sizing
    Now using JS to watch the window size and adjust the width/height of the chessboard wrapper with inline styles

--- src/components/ChessBoard.vue
+++ src/components/ChessBoard.vue
@@ -1,57 +1,65 @@
 <template>
-  <div class="chessboard-wrapper">
-    <div class="chessboard-file-gutter">
-      <div class="gutter-file-labels">
-        <div
-          v-for="file in filesArray"
-          :key="`label-file-${file}`"
-          :class="{ active: currentSelection.includes(file) }"
-          class="gutter-label"
-          v-html="file" />
-      </div>
-    </div>
-    <div class="chessboard-board">
-      <div class="gutter-rank-labels">
-        <div
-          v-for="rank in ranksArray"
-          :key="`label-rank-${rank}`"
-          :class="{ active: currentSelection.includes(rank) }"
-          class="gutter-label"
-          v-html="rank" />
-      </div>
-      <div class="board-grid">
-        <div
-          v-for="rank in ranksArray"
-          :key="`board-rank-${rank}`"
-          class="board-rank">
+  <div
+    ref="chessboard"
+    class="chessboard-wrapper">
+
+    <div
+      :style="chessboardStyle"
+      class="chessboard-board">
+      <div class="chessboard-file-gutter">
+        <div class="gutter-file-labels">
           <div
             v-for="file in filesArray"
-            :key="`board-${file}${rank}`"
-            :class="{ active: currentSelection === `${file}${rank}` }"
-            class="board-square"
-            @click="logClick({file, rank})"
-            v-html="currentSelection === `${file}${rank}` ? `${file}${rank}` : null" />
+            :key="`label-file-${file}`"
+            :class="{ active: currentSelection.includes(file) }"
+            class="gutter-label"
+            v-html="file" />
         </div>
       </div>
-      <div class="gutter-rank-labels">
-        <div
-          v-for="rank in ranksArray"
-          :key="`label-rank-${rank}`"
-          :class="{ active: currentSelection.includes(rank) }"
-          class="gutter-label"
-          v-html="rank" />
+      <div class="chessboard-board-grid">
+        <div class="gutter-rank-labels">
+          <div
+            v-for="rank in ranksArray"
+            :key="`label-rank-${rank}`"
+            :class="{ active: currentSelection.includes(rank) }"
+            class="gutter-label"
+            v-html="rank" />
+        </div>
+        <div class="board-grid">
+          <div
+            v-for="rank in ranksArray"
+            :key="`board-rank-${rank}`"
+            class="board-rank">
+            <div
+              v-for="file in filesArray"
+              :key="`board-${file}${rank}`"
+              :class="{ active: currentSelection === `${file}${rank}` }"
+              class="board-square"
+              @click="logClick({file, rank})"
+              v-html="currentSelection === `${file}${rank}` ? `${file}${rank}` : null" />
+          </div>
+        </div>
+        <div class="gutter-rank-labels">
+          <div
+            v-for="rank in ranksArray"
+            :key="`label-rank-${rank}`"
+            :class="{ active: currentSelection.includes(rank) }"
+            class="gutter-label"
+            v-html="rank" />
+        </div>
+      </div>
+      <div class="chessboard-file-gutter">
+        <div class="gutter-file-labels">
+          <div
+            v-for="file in filesArray"
+            :key="`label-file-${file}`"
+            :class="{ active: currentSelection.includes(file) }"
+            class="gutter-label"
+            v-html="file" />
+        </div>
       </div>
     </div>
-    <div class="chessboard-file-gutter">
-      <div class="gutter-file-labels">
-        <div
-          v-for="file in filesArray"
-          :key="`label-file-${file}`"
-          :class="{ active: currentSelection.includes(file) }"
-          class="gutter-label"
-          v-html="file" />
-      </div>
-    </div>
+
   </div>
 </template>
 
@@ -64,6 +72,10 @@ export default {
       files: 'abcdefgh',
       ranks: '12345678',
       currentSelection: '',
+      chessboardStyle: {
+        width: '',
+        height: '',
+      },
     };
   },
   computed: {
@@ -75,6 +87,13 @@ export default {
       return splitArray.reverse();
     },
   },
+  mounted() {
+    this.setChessboardSize();
+    window.addEventListener('resize', this.setChessboardSize);
+  },
+  beforeDestroy() {
+    window.removeEventListener('resize', this.setChessboardSize);
+  },
   methods: {
     ...mapActions(['addMoveToHistory']),
     logClick({ file, rank }) {
@@ -82,6 +101,16 @@ export default {
         this.currentSelection = `${file}${rank}`;
         const dateTimeStamp = Date.now();
         this.addMoveToHistory({ file, rank, dateTimeStamp });
+      }
+    },
+    setChessboardSize() {
+      const { clientWidth, clientHeight } = this.$refs.chessboard;
+      if (clientHeight < clientWidth) {
+        this.chessboardStyle.width = `${clientHeight}px`;
+        this.chessboardStyle.height = `${clientHeight}px`;
+      } else {
+        this.chessboardStyle.width = `${clientWidth}px`;
+        this.chessboardStyle.height = `${clientWidth}px`;
       }
     },
   },
